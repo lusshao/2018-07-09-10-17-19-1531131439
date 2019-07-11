3bb04e87878613d65d14326031c655d3cb4782ba
Liam Lu <luanyuanshizhu@126.com>
2019-07-11 23:21:08 +0800

    update and test practice07/Person.java,practice07/Student.java , practice07/Teacher.java and practice07/Klass.java

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,29 @@
 package practice07;
 
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+
+    public boolean equals(Klass klass){
+        if(this.number == klass.getNumber()){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,36 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private Klass klass;
+    public Teacher(String name, int age){
+        super(name,age);
+    }
+    public Teacher(String name, int age,Klass klass){
+        super(name,age);
+        this.klass = klass;
+    }
+    public String introduce(){
+        if(klass!=null){
+            return super.introduce()+" I am a Teacher. I teach Class "+klass.getNumber()+".";
+        }else {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+    }
+
+    public String introduceWith(Student student ){
+        if(klass.equals(student.getKlass())){
+            return super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+        }else {
+            return super.introduce() + " I am a Teacher. I don't teach "+student.getName()+".";
+        }
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
