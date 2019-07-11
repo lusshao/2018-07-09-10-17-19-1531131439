331b075a253de8a978a9a260d9669ea434d99971
Liam Lu <luanyuanshizhu@126.com>
2019-07-11 22:39:30 +0800

    update and test practice06/Person.java,practice06/Student.java and practice06/Teacher.java

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private int klass;
+    public Teacher(String name, int age){
+        super(name,age);
+    }
+    public Teacher(String name, int age,int klass){
+        super(name,age);
+        this.klass = klass;
+    }
+    public String introduce(){
+        if(klass!=0){
+            return super.introduce()+" I am a Teacher. I teach Class "+klass+".";
+        }else {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }
