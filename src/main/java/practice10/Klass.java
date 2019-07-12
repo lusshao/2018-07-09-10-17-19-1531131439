1f66ecf708318e3b98a2f92ad66c7b213c933a28
LULI3 <luanyuanshizhu@126.com>
2019-07-12 08:36:03 +0800

    update and test practice10/Person.java,practice10/Student.java , practice10/Klass.java. , practice10/Teacher.java

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,45 @@
 package practice10;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+    public void assignLeader(Student student){
+        if(student.getKlass() != this){
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        leader=student;
+    }
+
+    public boolean equals(Klass klass){
+        if(this.number == klass.getNumber()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void appendMember(Student student){
+        student.setKlass(this);
+    }
 }
